c6ead0f53c302a2e8d126c6c67b7a0801075d7b5
kevin.carvalho <kevin.folc@gmail.com>
2021-10-27 14:59:18 -0400

    comit

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ url = "http://bytebank.com/cambio?moedaOrigem=real"
 indice_interrogacao = url.find("?")
 url_base = url[:indice_interrogacao]
 
-url_parametro = url[indice_interrogacao+1:]
-print(url_parametro)
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
 
 
 parametro_busca = "moedaOrigem"
-indice_parametro = url_parametro.find(parametro_busca)
+indice_parametro = url_parametros.find(parametro_busca)
 print(indice_parametro)
 
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor=  url_parametro[indice_valor:]
+valor=  url_parametros[indice_valor:]
 print(valor)
